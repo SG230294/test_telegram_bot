95ba5026ab0fe0cf991a0136b1fe945e42778541
Slava <sg230294@gmail.com>
2021-12-28 17:18:12 +0300

    Обработка ошибок

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,24 @@
-//import 'source-map-support/register'
+import 'source-map-support/register'
 import {Telegraf} from 'telegraf'
 import {evaluate} from 'mathjs'
 
 const tgToken = "5006553438:AAGtfisPFLv71VIiMj7Rzk-x63_BjJYJ4hg"
 const bot = new Telegraf(tgToken)
 
-bot.on('text', (ctx) => ctx.reply('test'))
+bot.on('text', (ctx) => ctx.reply(tryToEvaluate(ctx.message.text.toString())));
+
+function tryToEvaluate(str: string) {
+    let result: string;
+    try {
+        result = evaluate(str);
+    }
+    catch(e){
+        result = 'error';
+        console.log(e);
+    }
+    return result;
+}
+
 bot.launch()
 
 console.log('Done!')
